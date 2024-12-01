b6d6296e41904ce3beda364478f4b157b4bb4bfa
Solnyshka1 <saadat.nurbekova1@alatoo.edu.kg>
2024-12-01 15:23:02 +0600

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.jdbc {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.jdbc to javafx.fxml;
     exports com.example.jdbc;
